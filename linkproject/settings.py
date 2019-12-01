c21ff1a18dde2798587e96b3e98dd1538f7c2051
Isaacg94 <7248zack@gmail.com>
2019-12-01 20:38:33 +0300

    define app's configurations in project settings

--- linkproject/settings.py
+++ linkproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'linkapp.apps.LinkappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
